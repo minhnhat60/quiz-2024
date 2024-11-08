aabac2c20925cb54911cb46f154df3f7f8df7f64
chaotic <mnrealest161@gmail.com>
2024-11-09 06:03:22 +0700

    layout login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,7 +1,16 @@
+import "./Login.scss";
+
 const Login = () => {
     return (
         <>
-            <h2>Trang Login</h2>
+            <div className="form">
+                <h3 className="form__title">Login Quiz</h3>
+                <form>
+                    <input name="email" type="email" placeholder="Nhập Email" required></input>
+                    <input name="password" type="password" placeholder="Nhập Password" required></input>
+                    <button type="submit" className="button button__main">Login</button>
+                </form>
+            </div>
         </>
     )
 }
