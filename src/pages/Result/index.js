9060daff38927df24e97460710854a64bb102f87
chaotic <mnrealest161@gmail.com>
2024-11-14 23:11:06 +0700

    build page result n answers from user

--- src/pages/Result/index.js
+++ src/pages/Result/index.js
@@ -13,6 +13,7 @@ const Result = () => {
             const dataQuestions = await getListQuestion(dataAnswers.topicId);
 
             const dataFinal = [];
+
             for(let i = 0; i < dataQuestions.length; i++) {
                 const questionId = dataQuestions[i].id;
                 const objectAnswer = dataAnswers.answers.find(item => item.questionId === questionId);
@@ -23,15 +24,50 @@ const Result = () => {
                 });
             }
 
-            console.log(dataFinal);
+            setDataResult(dataFinal);
         }
 
         fetchApi();
-    }, []);
+    }, [params.id]);
 
     return (
         <>
             <h2>Trang Result</h2>
+
+            {dataResult && (
+                <div className="result">
+                    {dataResult.map((item, indexItem) => (
+                        <div className="result__item" key={indexItem}>
+                            <p>
+                                Câu {indexItem + 1} : {item.question}
+                            </p>
+
+                            {item.answers.map((answer, indexAnswer) => {
+                                let className = "";
+                                let checked = false;
+
+                                if(indexAnswer === item.answer) {
+                                    checked = true;
+                                    className = "result__item--selected";
+                                }
+
+                                if(indexAnswer === item.correctAnswer) {
+                                    className = "result__item--selected";
+                                }
+
+                                return (
+                                    <div key={indexAnswer}>
+                                        <input type="radio" disabled checked={checked} />
+                                        <label className={className}>
+                                            {answer}
+                                        </label>
+                                    </div>
+                                )
+                            })}
+                        </div>
+                    ))}
+                </div>
+            )}
         </>
     )
 }
